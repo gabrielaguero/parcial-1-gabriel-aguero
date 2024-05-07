ce8cd4e026874fdd01f41c14ec6100585da5a196
Gabriel Agüero <91287456+gabrielaguero@users.noreply.github.com>
2024-05-06 23:10:37 -0300

    Delete aguero-gabriel-tp1 directory

--- aguero-gabriel-tp1/middleware/authenticator.js
+++
@@ -1,9 +0,0 @@
-const authenticate = (req, res, next) => {
-    const token = req.query.token;
-    if (!token || token !== "mi_token_secreto") {
-        return res.status(401).json({ message: "Acceso no autorizado" });
-    }
-    next();
-};
-
-export default authenticate;
